bc579386d1790c22045f382f3b898ce7b04f0b2c
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-14 11:45:10 +0200

    Feat(DataRow): add click functionality

--- src/components/DataRow/DataRow.tsx
+++ src/components/DataRow/DataRow.tsx
@@ -2,9 +2,12 @@ import { DataRowProps } from '../../models/propertyModels/componentsProperties';
 import './DataRow.css';
 
 function DataRow({data}: DataRowProps) {
+    function clickHandler() {
+        alert("Hey listen!");
+    }
 
     return(
-        <div className='table-row'>
+        <div className='table-row' onClick={clickHandler}>
             <p>{data.tenantName}</p>
             <p>{data.cif}</p>
             <p>{data.phone}</p>
