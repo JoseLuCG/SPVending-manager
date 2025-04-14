56e8d180dfa569b5cc1f00780e00f96b865f5a9d
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-14 11:57:28 +0200

    Feat(components): create a new component

---
+++ src/components/TableHeader/TableHeader.tsx
@@ -0,0 +1,16 @@
+import './TableHeader.css';
+
+function TableHeader() {
+    return (
+        <div className='table-header'>
+            <p>Name</p>
+            <p>CIF</p>
+            <p>Phone</p>
+            <p>Email</p>
+            <p>Clubs</p>
+            <p>Actions</p>
+        </div>
+    );
+}
+
+export default TableHeader;
