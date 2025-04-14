631242bd44f93f3e82d453481bd98e19b50c20b5
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-14 11:16:01 +0200

    Feat(models): create a new file for properties components

---
+++ src/models/propertyModels/componentsProperties.ts
@@ -0,0 +1,13 @@
+import { Tenant } from '../../models/dataModels/tenant';
+
+export type DataRowProps = {
+    data: Tenant
+}
+
+export type NavButtonsProps = {
+    isAbove: boolean;
+}
+
+export type MenuButtonProps = {
+    isAbove: boolean;
+};
