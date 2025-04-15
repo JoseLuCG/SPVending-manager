a8f161879b0e4a3f2519fcb1ab3eea01777db563
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-15 15:29:58 +0200

    Refactor(component): use the `DataTable` component of primereact

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,60 +3,72 @@ import { Tenant, TenantList } from '../../models/dataModels/tenant';
 //import searchIcon from './../../assets/icons/Search-more.svg';
 import Aside from './../Aside/Aside';
 import { useEffect, useState } from 'react';
-import DataRow from '../DataRow/DataRow';
-import TableHeader from '../TableHeader/TableHeader';
+//import DataRow from '../DataRow/DataRow';
+//import TableHeader from '../TableHeader/TableHeader';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 function Main() {
-  //States:
-  const [ tenants, setTenants ] = useState<TenantList>([]);
+	//States:
+	const [tenants, setTenants] = useState<TenantList>([]);
 
-  //Functions:
-  function createArrayOfTenantsExamples() {
-      const tenantsArray: TenantList = [];
-      const tenantExample: Tenant= {
-        tenantId: "a81bc81b-dead-4e5d-abff-90865d1e13b1",
-        tenantName: "PadelPrix",
-        cif: 123456789,
-        address: "Example address N2 45",
-        phone: 611222333,
-        email: "padelprix@gmail.com",
-        remark: "empty",
-        micronId: "12345678",
-        numberOfClubs: 4
-      };
+	//Functions:
+	function createArrayOfTenantsExamples() {
+		const tenantsArray: TenantList = [];
+		const tenantExample: Tenant = {
+			tenantId: "a81bc81b-dead-4e5d-abff-90865d1e13b1",
+			tenantName: "PadelPrix",
+			cif: 123456789,
+			address: "Example address N2 45",
+			phone: 611222333,
+			email: "padelprix@gmail.com",
+			remark: "empty",
+			micronId: "12345678",
+			numberOfClubs: 4
+		};
 
-      for (let idx = 0; idx < 5; idx++) {
-          tenantsArray.push(tenantExample);
-      }
-      setTenants(tenantsArray);
-  }
+		for (let idx = 0; idx < 5; idx++) {
+			tenantsArray.push(tenantExample);
+		}
+		setTenants(tenantsArray);
+	}
 
-  useEffect(
-    () => {
-      createArrayOfTenantsExamples();
-    }
-  );
+	// ----- Styles -----
+	const rowClassName = () => {
+		return {
+			"table-row": true,
+			"p-datatable-thead": true
+		};
+	};
 
-  return (
-    <main>
-        <Aside/>
-        <section>
-            <div className='table-contents'>
-                <div className='header-content'>
-                    <p className='infText'>Tenant List</p>
-                    <button className='addButton'>+ Add tenant</button>
-                    <input className='searcher' type="text" placeholder='Buscar...'/>
-                </div>
-                <TableHeader/>
-                {
-                   tenants.map(
-                    (tenantData: Tenant) => <DataRow data={tenantData} />
-                  )
-                }
-            </div>
-        </section>
-    </main>
-  )
+	useEffect(
+		() => {
+			createArrayOfTenantsExamples();
+		}
+	);
+
+	return (
+		<main>
+			<Aside />
+			<section>
+				<div className='table-contents'>
+					<div className='header-content'>
+						<p className='infText'>Tenant List</p>
+						<button className='addButton'>+ Add tenant</button>
+						<input className='searcher' type="text" placeholder='Buscar...' />
+					</div>
+					<DataTable value={tenants} selectionMode="single" rowClassName={rowClassName}>
+						<Column field="tenantName" header="Name" className='headerRow'></Column>
+						<Column field="cif" header="CIF"></Column>
+						<Column field="phone" header="Phone"></Column>
+						<Column field="email" header="Email"></Column>
+						<Column field="numberOfClubs" header="Clubs"></Column>
+						<Column field="" header="Actions"></Column>
+					</DataTable>
+				</div>
+			</section>
+		</main>
+	)
 }
 
 export default Main;
