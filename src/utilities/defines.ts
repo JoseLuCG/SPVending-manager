24f3e2b0766a4779c27f610c4cf9c4f93ba7f7e1
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-15 19:42:25 +0200

    Feat(utilities): add a defines file to change global data

---
+++ src/utilities/defines.ts
@@ -0,0 +1,45 @@
+export const infoDisplayTenant = {
+    column1: {
+        field: "tenantName",
+        header: "Name"
+    },
+    column2: {
+        field: "cif",
+        header: "CIF"
+    },
+    column3: {
+        field: "phone",
+        header: "Phone"
+    },
+    column4: {
+        field: "email",
+        header: "Email"
+    },
+    column5: {
+        field: "numberOfClubs",
+        header: "Clubs"
+    }
+};
+
+export const infoDisplayClub = {
+    column1: {
+        field: "clubName",
+        header: "Name"
+    },
+    column2: {
+        field: "tenantName",
+        header: "Tenant"
+    },
+    column3: {
+        field: "numberOfMachines",
+        header: "Machines"
+    },
+    column4: {
+        field: "address",
+        header: "Address"
+    },
+    column5: {
+        field: "phone",
+        header: "Phone"
+    }
+};
