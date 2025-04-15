c3864361eeedad0283041cadd2a7049575b9ba7a
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-15 19:39:47 +0200

    Feat(Main component): add new example data and add the DataTable of primereact library

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import './Main.css';
 import { Tenant, TenantList } from '../../models/dataModels/tenant';
+import { Club, ClubList } from '../../models/dataModels/club';
 //import searchIcon from './../../assets/icons/Search-more.svg';
 import Aside from './../Aside/Aside';
 import { useEffect, useState } from 'react';
@@ -7,14 +8,31 @@ import { useEffect, useState } from 'react';
 //import TableHeader from '../TableHeader/TableHeader';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { MainProps } from '../../models/propertyModels/componentsProperties';
 
-function Main() {
+function Main({infoDisplay}: MainProps) {
 	//States:
-	const [tenants, setTenants] = useState<TenantList>([]);
+	const [ tenants, setTenants ] = useState<TenantList>([]);
+	const [ clubs, setClubs ] = useState<ClubList>([]);
 
 	//Functions:
-	function createArrayOfTenantsExamples() {
+	function createArrayOfTenantsExamplesOrClubExamples() {
 		const tenantsArray: TenantList = [];
+		const clubsArray: ClubList = [];
+		const clubExample: Club = {
+			clubId: "a81bc81b-dead-4e5d-abff-90865d1e13b1",
+			clubName: "PadelPrix Ourense",
+			cif: 123456789,
+			address: "Rua Camiños N34 P4D",
+			phone: 622333444,
+			email: "clubou@gmail.com",
+			remark: "emtpy",
+			micronId: "VFW1234",
+			accountingId: "HOLAMUNDO",
+			tenantName: "PadelPrix",
+			numberOfMachines: 5
+		}
+		
 		const tenantExample: Tenant = {
 			tenantId: "a81bc81b-dead-4e5d-abff-90865d1e13b1",
 			tenantName: "PadelPrix",
@@ -28,9 +46,13 @@ function Main() {
 		};
 
 		for (let idx = 0; idx < 5; idx++) {
+			clubsArray.push(clubExample);
+		}
+		for (let idx = 0; idx < 5; idx++) {
 			tenantsArray.push(tenantExample);
 		}
 		setTenants(tenantsArray);
+		setClubs(clubsArray);
 	}
 
 	// ----- Styles -----
@@ -41,11 +63,21 @@ function Main() {
 		};
 	};
 
+	// ----- Functions -----
+	function result() {
+		if (infoDisplay.column1.field == "tenantName") {
+			return tenants;
+		} else {
+			return clubs;
+		}
+	}
+
 	useEffect(
 		() => {
-			createArrayOfTenantsExamples();
-		}
+			createArrayOfTenantsExamplesOrClubExamples();			
+		}, []
 	);
+	
 
 	return (
 		<main>
@@ -57,12 +89,12 @@ function Main() {
 						<button className='addButton'>+ Add tenant</button>
 						<input className='searcher' type="text" placeholder='Buscar...' />
 					</div>
-					<DataTable value={tenants} selectionMode="single" rowClassName={rowClassName}>
-						<Column field="tenantName" header="Name" className='headerRow'></Column>
-						<Column field="cif" header="CIF"></Column>
-						<Column field="phone" header="Phone"></Column>
-						<Column field="email" header="Email"></Column>
-						<Column field="numberOfClubs" header="Clubs"></Column>
+					<DataTable value={result()} selectionMode="single" rowClassName={rowClassName}>
+						<Column field={infoDisplay.column1.field} header={infoDisplay.column1.header}></Column>
+						<Column field={infoDisplay.column2.field} header={infoDisplay.column2.header}></Column>
+						<Column field={infoDisplay.column3.field} header={infoDisplay.column3.header}></Column>
+						<Column field={infoDisplay.column4.field} header={infoDisplay.column4.header}></Column>
+						<Column field={infoDisplay.column5.field} header={infoDisplay.column5.header}></Column>
 						<Column field="" header="Actions"></Column>
 					</DataTable>
 				</div>
