50ba3d032eb68d122b25d52ff993600ab2159417
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-15 19:34:16 +0200

    Feat(models): add the new model `club`

---
+++ src/models/dataModels/club.ts
@@ -0,0 +1,15 @@
+export type Club = {
+    clubId: string,
+    clubName: string,
+    cif: number,
+    address: string,
+    phone: number,
+    email: string,
+    remark: string,
+    micronId: string,
+    accountingId: string
+    tenantName: string,
+    numberOfMachines: number
+}
+
+export type ClubList = Club[];
