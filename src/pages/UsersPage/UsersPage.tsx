3663669cb1c54e41d4b7cbd48019e2bbbab2ff07
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-16 12:40:29 +0200

    Feat(pages): add the `UsersPage`

---
+++ src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,16 @@
+import './UsersPage.css';
+import Main from './../../components/Main/Main';
+import Header from './../../components/Header/Header';
+import { infoDisplayUsers } from '../../utilities/infoDisplay';
+
+function UsersPage() {
+
+	return (
+		<>
+			<Header />
+			<Main infoDisplay={ infoDisplayUsers } />
+		</>
+	)
+}
+
+export default UsersPage;
