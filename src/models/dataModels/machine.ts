35b2cceb16946168fdcb8936156dc01667462ab9
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-16 10:34:50 +0200

    Feat(models): add machine model

---
+++ src/models/dataModels/machine.ts
@@ -0,0 +1,12 @@
+export type Machine = {
+    machineId : string,
+    machineCode: string,
+    micronId: string,
+    smartFridgeld: string,
+    smartFridgePassword : string,
+    terminalId: number,
+    tnaSerialNumber: string
+    rustdeskId: string,
+    rustdeskPass: string,
+    clubName: string
+}
