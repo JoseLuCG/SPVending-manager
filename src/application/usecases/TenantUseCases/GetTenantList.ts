56b4b1d36191a6b220f07d45ff9abc3dbcf5ee35
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-23 13:17:42 +0200

    Feat(models): add new specific model to tenant entity and correct the type of execute method

--- src/application/usecases/TenantUseCases/GetTenantList.ts
+++ src/application/usecases/TenantUseCases/GetTenantList.ts
@@ -1,11 +1,11 @@
 import { TenantRepository } from "../../../domain/ports/TenantRepository";
-import { Tenant } from "../../../domain/entities/models/tenant";
+import { TenantInfoDisplay } from "../../../domain/entities/models/tenant";
 
 export class GetTenantList {
     constructor( private tenantRepository: TenantRepository ){};
 
-    async execute(): Promise<Tenant[]> {
-        const tenants = await this.tenantRepository.getAllTenants();
+    async execute(): Promise<TenantInfoDisplay[]> {
+        const tenants: TenantInfoDisplay[] = await this.tenantRepository.getAllTenants();
 
         if(!tenants) throw new Error("Tenant list is not available");
 
--- src/domain/entities/models/tenant.ts
+++ src/domain/entities/models/tenant.ts
@@ -1,11 +1,20 @@
 export type Tenant = {
-  tenantId: string,
-  tenantName: string,
-  cif: number,
-  address: string,
-  phone: number,
-  email: string,
-  remark: string,
-  micronId: string
-  numberOfClubs: number
+	tenantId: string,
+	tenantName: string,
+	cif: number,
+	address: string,
+	phone: number,
+	email: string,
+	remark: string,
+	micronId: string
+	numberOfClubs: number
+}
+
+export type TenantInfoDisplay = {
+	tenantId: string;
+	name: string;
+	cif: string;
+	phone: string;
+	email: string;
+	clubsCount: number;
 }
