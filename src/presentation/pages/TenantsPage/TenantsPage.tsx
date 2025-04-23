6f5f7c807bc5884a65f76a71e2a488e2b07c60b4
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-23 13:43:02 +0200

    Feat(page): Update the `TenantPage` to be able to make requests to the backend

--- src/presentation/pages/TenantsPage/TenantsPage.tsx
+++ src/presentation/pages/TenantsPage/TenantsPage.tsx
@@ -2,13 +2,27 @@ import './TenantsPage.css';
 import Main from './../../components/Main/Main';
 import Header from './../../components/Header/Header';
 import { infoDisplayTenant } from '../../../utilities/infoDisplay';
+import { useEffect, useState } from 'react';
+import { TenantInfoDisplay } from '../../../domain/entities/models/tenant';
+import { TenantRepositoryHttp } from '../../../infraestructure/adapters/api/TenantRepositoryHttp';
+import { GetTenantList } from '../../../application/usecases/TenantUseCases/GetTenantList';
+
+const repository = new TenantRepositoryHttp();
+const getTenantList = new GetTenantList(repository);
 
 function TenantsPage() {
+	const [ tenants , setTenants ] = useState<TenantInfoDisplay[]>([]);
+
+	useEffect(()=> {
+		getTenantList.execute()
+			.then(setTenants)
+			.catch(console.error);
+	}, []);
 
 	return (
 		<>
 			<Header />
-			<Main infoDisplay={infoDisplayTenant} />
+			<Main textInfoDisplay={infoDisplayTenant} dataToDisplay={tenants} />
 		</>
 	)
 }
