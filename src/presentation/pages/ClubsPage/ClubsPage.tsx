8192e727f32138728348f52c43de5e05c3a3b384
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-25 12:21:45 +0200

    Feat(pages): update the club page with functionality

--- src/presentation/pages/ClubsPage/ClubsPage.tsx
+++ src/presentation/pages/ClubsPage/ClubsPage.tsx
@@ -2,13 +2,27 @@ import './ClubsPage.css';
 import Main from './../../components/Main/Main';
 import Header from './../../components/Header/Header';
 import { infoDisplayClub } from '../../../utilities/infoDisplay';
+import { ClubRepositoryHttp } from '../../../infraestructure/adapters/api/ClubRepositoryHttp';
+import { GetClubList } from '../../../application/usecases/ClubUseCases/GetClubList';
+import { useEffect, useState } from 'react';
+import { ClubInfoDisplay } from '../../../domain/entities/models/club';
+
+const repository = new ClubRepositoryHttp();
+const getClubList = new GetClubList(repository);
 
 function ClubsPage() {
+	const [ clubs, setClubs ] = useState<ClubInfoDisplay[]>([]);
+
+	useEffect(()=>{
+		getClubList.execute()
+			.then(setClubs)
+			.catch(console.error);
+	}, []);
 
 	return (
 		<>
 			<Header />
-			<Main textInfoDisplay={infoDisplayClub} />
+			<Main textInfoDisplay={infoDisplayClub} dataToDisplay={clubs}/>
 		</>
 	)
 }
