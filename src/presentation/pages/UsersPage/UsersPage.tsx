658f333de205f0e26ebf319ab97a13981efbdf31
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-28 10:05:27 +0200

    Feat(pages): add functionality to user page

--- src/presentation/pages/UsersPage/UsersPage.tsx
+++ src/presentation/pages/UsersPage/UsersPage.tsx
@@ -2,13 +2,27 @@ import './UsersPage.css';
 import Main from './../../components/Main/Main';
 import Header from './../../components/Header/Header';
 import { infoDisplayUsers } from './../../../utilities/infoDisplay';
+import { UserRepositoryHttp } from '../../../infraestructure/adapters/api/UserRepositoryHttp';
+import { GetUserList } from '../../../application/usecases/UserUseCases/GetUserList';
+import { useEffect, useState } from 'react';
+import { UserInfoDisplay } from '../../../domain/entities/models/user';
+
+const repository = new UserRepositoryHttp();
+const getUserList = new GetUserList(repository);
 
 function UsersPage() {
+	const [ users, setUsers ] = useState<UserInfoDisplay[]>([]);
+
+	useEffect(()=> {
+		getUserList.execute()
+			.then(setUsers)
+			.catch(console.error);
+	},[]);
 
 	return (
 		<>
 			<Header />
-			<Main textInfoDisplay={ infoDisplayUsers } />
+			<Main textInfoDisplay={ infoDisplayUsers } dataToDisplay={users}/>
 		</>
 	)
 }
