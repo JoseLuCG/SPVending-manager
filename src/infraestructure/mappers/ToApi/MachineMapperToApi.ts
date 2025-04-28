57a1c6d838c8987d52912afcc0878b6a8f910a79
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-28 09:25:43 +0200

    Feat(mappers): create  mapper

---
+++ src/infraestructure/mappers/ToApi/MachineMapperToApi.ts
@@ -0,0 +1,15 @@
+import { Machine } from "../../../domain/entities/models/machine";
+
+export function mapMachineToApi(machine: Partial<Machine>): any {
+    return {
+        code: machine.machineCode,
+        micronId: machine.micronId,
+        smartFridgeId: machine.smartFridgeld,
+        smartFridgePassword : machine.smartFridgePassword,
+        terminalId: machine.terminalId,
+        toaSerialNumber: machine.tnaSerialNumber,
+        rustdeskId: machine.rustdeskId,
+        rustdeskPass: machine.rustdeskPass,
+        clubId: machine.clubId
+    };
+}
