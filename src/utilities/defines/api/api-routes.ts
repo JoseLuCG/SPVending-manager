1926236845c5b1bbe5db5550484873596e536f14
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-28 08:49:17 +0200

    Feat(utilities): add `machinesPath` to `PATH_PREFIX` constant

--- src/utilities/defines/api/api-routes.ts
+++ src/utilities/defines/api/api-routes.ts
@@ -3,5 +3,6 @@ export const API_PREFIX = "/api/v1";
 export const PATH_PREFIX = {
     tenantPath: "/tenants",
     clubPath: "/clubs",
-    potentialTenants: "all-sumary"
+    potentialTenants: "all-sumary",
+    machinesPath: "/machines"
 }
