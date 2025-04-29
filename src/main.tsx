9891526d93b15fa72666a9afaa1e34d936be349c
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 16:59:30 +0200

    Feat(app): implement the context for the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import { SelectedItemContext } from './contexts/SelectedItemContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
 		<StrictMode>
-			<App />
+			<SelectedItemContext>
+				<App />
+			</SelectedItemContext>
 		</StrictMode>
 	</BrowserRouter>
 )
