cdc59dad8ca68b583246e3b29f4ef2896daa9d3d
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 13:26:36 +0200

    Feat(components): implement the `MachineRegisterModal` component to Modal component

--- src/presentation/components/Modals/Modal.tsx
+++ src/presentation/components/Modals/Modal.tsx
@@ -1,12 +1,13 @@
 import { GenericModalProps } from "../../../domain/entities/property-models/componentsProperties";
 import ClubRegisterModal from "./ClubRegisterModal/ClubRegisterModal";
+import MachineRegisterModal from "./MachineRegisterModal/MachineRegisterModal";
 import TenantRegisterModal from "./TenantRegisterModal/TenantRgisterModal";
 import UserRegisterModal from "./UserRegisterModal/UserRegisterModal";
 
 function Modal({ typeModal, isOpen, onClose}:GenericModalProps) {
     if (typeModal == "Tenant") return <TenantRegisterModal isOpen={isOpen} onClose={onClose}/>;
     if (typeModal == "Club") return <ClubRegisterModal isOpen={isOpen} onClose={onClose} />;
-    if (typeModal == "Machine");
+    if (typeModal == "Machine") return <MachineRegisterModal isOpen={isOpen} onClose={onClose}/>;
     if (typeModal == "User") return <UserRegisterModal isOpen={isOpen} onClose={onClose} />;
 }
 
