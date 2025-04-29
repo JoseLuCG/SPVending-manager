8a3033669e7ba7de1067573201d620910c25a950
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 11:57:24 +0200

    Feat(mappers): create the `mapUserToApi` mapper

---
+++ src/infraestructure/mappers/ToApi/UserMapperToApi.ts
@@ -0,0 +1,13 @@
+import { User } from "../../../domain/entities/models/user";
+
+export function mapUserToApi(user: Partial<User>): any {
+    return {
+      username: user.username,
+      password: user.password,
+      micronId: user.micronId,
+      micronUser: user.micronUser,
+      micronPass: user.micronPass,
+      userType: user.userType,
+      tenantEntityId: user.tenantId
+    };
+}
