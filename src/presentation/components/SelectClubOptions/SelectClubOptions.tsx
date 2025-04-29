5c42e5799f8b71f428301a91cff8a966e4453474
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 09:01:05 +0200

    Feat(components): create `SelectClubOptions` component and its css file

---
+++ src/presentation/components/SelectClubOptions/SelectClubOptions.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { GetClubList } from "../../../application/usecases/ClubUseCases/GetClubList";
+import { SelectClubOptionProps } from "../../../domain/entities/property-models/componentsProperties";
+import { ClubRepositoryHttp } from "../../../infraestructure/adapters/api/ClubRepositoryHttp";
+import { ClubOption } from "../../../domain/entities/models/club";
+
+const clubRepo = new ClubRepositoryHttp();
+const getClubOptions = new GetClubList(clubRepo);
+
+function SelectClubOptions({onSelectClub}: SelectClubOptionProps) {
+    // States:
+    const [ clubsOptions, setClubOptions ] = useState<ClubOption[]>([]);
+    const [ selectOpen, setSelectOpen ] = useState(false);
+
+    // Handlers:
+    async function fetchClubs() {
+        const response = await getClubOptions.execute();
+        setClubOptions(response);
+    }
+
+    function onClickHandler() {
+        if (!selectOpen) {
+            fetchClubs();
+            setSelectOpen(true);
+        }
+    }
+
+    return(
+        <>
+            <label htmlFor="clubs">Choose a club:</label>
+            <br />
+            <select name="clubEntityId" id="club-select" onClick={onClickHandler} onBlur={()=> setSelectOpen(false)} onChange={onSelectClub}>
+                <option>----- -----</option>
+                {
+                    clubsOptions.map(
+                        (c) => <option key={c.clubId} value={c.clubId}>{c.name}</option>
+                    )
+                }
+            </select>
+        </>
+    );
+}
+
+export default SelectClubOptions;
