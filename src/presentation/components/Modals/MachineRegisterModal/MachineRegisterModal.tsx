875c77fc1b96465a2e6c6814582adef9117b13a3
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 13:28:31 +0200

    Feat(components): add a mapper for changeHandler and add the SelectClubOptions component

--- src/presentation/components/Modals/MachineRegisterModal/MachineRegisterModal.tsx
+++ src/presentation/components/Modals/MachineRegisterModal/MachineRegisterModal.tsx
@@ -4,6 +4,7 @@ import { ModalProps } from "../../../../domain/entities/property-models/componen
 import { MachineRepositoryHttp } from "../../../../infraestructure/adapters/api/MachineRepositoryHttp";
 import "./../Modal.css";
 import { Machine } from "../../../../domain/entities/models/machine";
+import SelectClubOptions from "../../SelectClubOptions/SelectClubOptions";
 
 const machineRepository = new MachineRepositoryHttp();
 const createMachine = new CreateMachine(machineRepository);
@@ -25,9 +26,10 @@ function MachineRegisterModal({ isOpen, onClose }: ModalProps) {
     // Handlers:
     function changeHandler(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
         const { name, value } = event.target;
+        const mappedName = name === "clubEntityId"? "clubId":name;
         setMachineFormData({
             ...machineFormData,
-            [name]: name === "terminalId" ? Number(value) : value
+            [mappedName]: mappedName === "terminalId" ? Number(value) : value
         });
     }
 
@@ -49,16 +51,72 @@ function MachineRegisterModal({ isOpen, onClose }: ModalProps) {
             <div className="modal">
                 <h2>Register machine</h2>
                 <form onSubmit={submitHandler} className="form">
-                    <input name="machineCode" />
-                    <input name="micronId" />
-                    <input name="smartFridgeld" />
-                    <input name="smartFridgePassword" />
-                    <input name="terminalId" />
-                    <input name="tnaSerialNumber" />
-                    <input name="rustdeskId" />
-                    <input name="rustdeskPass" />
+                    <input
+                        name="machineCode"
+                        value={machineFormData.machineCode}
+                        onChange={changeHandler}
+                        placeholder="Machine code"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="micronId"
+                        value={machineFormData.micronId}
+                        onChange={changeHandler}
+                        placeholder="Micron ID"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="smartFridgeld"
+                        value={machineFormData.smartFridgeld}
+                        onChange={changeHandler}
+                        placeholder="Smart Fridgeld"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="smartFridgePassword"
+                        value={machineFormData.smartFridgePassword}
+                        onChange={changeHandler}
+                        placeholder="Smart Fridgeld Password"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="terminalId"
+                        value={machineFormData.terminalId}
+                        onChange={changeHandler}
+                        placeholder="Terminal ID"
+                        type="number"
+                        required
+                    />
+                    <input
+                        name="tnaSerialNumber"
+                        value={machineFormData.tnaSerialNumber}
+                        onChange={changeHandler}
+                        placeholder="Terminal Serial Number"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="rustdeskId"
+                        value={machineFormData.rustdeskId}
+                        onChange={changeHandler}
+                        placeholder="RustDesk ID"
+                        type="text"
+                        required
+                    />
+                    <input
+                        name="rustdeskPass"
+                        value={machineFormData.rustdeskPass}
+                        onChange={changeHandler}
+                        placeholder="RustDesk password"
+                        type="text"
+                        required
+                    />
                     <br />
-
+                    <SelectClubOptions onSelectClub={changeHandler}/>
                     <button type="submit">Registrar</button>
                     <button type="button" onClick={onClose}>Cancelar</button>
                 </form>
