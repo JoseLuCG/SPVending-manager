97257a83ca20f99c8dca06372bba835833de0221
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 12:07:19 +0200

    Refactor(component): incorporate the following changes:
    - Added the `clubName` property to the list of ignored
    properties in useState userForm.
    - Changed the "type" property name to "userType" name
    and change the check in the changeHandler function.
    - Added the input "type" attributes.
    - Added a input for "micronUser" property.
    - Added the radio buttons for the userType.

--- src/presentation/components/Modals/UserRegisterModal/UserRegisterModal.tsx
+++ src/presentation/components/Modals/UserRegisterModal/UserRegisterModal.tsx
@@ -1,3 +1,4 @@
+import "./../Modal.css"
 import { useState } from "react";
 import { CreateUser } from "../../../../application/usecases/UserUseCases/CreateUser";
 import { ModalProps } from "../../../../domain/entities/property-models/componentsProperties";
@@ -12,27 +13,26 @@ const createUser = new CreateUser(userRepo);
 
 function UserRegisterModal({ isOpen, onClose }: ModalProps) {
     // States:
-    const [ userForm, setUserForm ] = useState<Omit<User,"userId">>({
+    const [userForm, setUserForm] = useState<Omit<User, "userId"|"clubName">>({
         username: "",
         password: "",
         micronId: "",
         micronUser: "",
         micronPass: "",
-        type: 1,
-        clubName: "",
+        userType: 1,
         tenantId: ""
     });
 
     //Handlers:
-    function changeHandler( event:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
-        const {name, value} = event.target;
+    function changeHandler(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+        const { name, value } = event.target;
         setUserForm({
             ...userForm,
-            [name]: name === "type"? Number(value) : value
+            [name]: name === "userType" ? Number(value) : value
         });
     }
 
-    async function submitHandler(event:React.FormEvent) {
+    async function submitHandler(event: React.FormEvent) {
         event.preventDefault();
         try {
             await createUser.execute(userForm);
@@ -45,18 +45,43 @@ function UserRegisterModal({ isOpen, onClose }: ModalProps) {
 
     if (!isOpen) return null;
 
-    return(
+    return (
         <div className="modal-overlay">
             <div className="modal">
                 <h2>Registear User</h2>
                 <form onSubmit={submitHandler} className="form">
-                    <input name="username" placeholder="User name" value={userForm.username} onChange={changeHandler} required />
-                    <input name="password" placeholder="User password" value={userForm.password} onChange={changeHandler} required />
+                    <input name="username" type="text" placeholder="User name" value={userForm.username} onChange={changeHandler} required />
+                    <input name="password" type="password" placeholder="User password" value={userForm.password} onChange={changeHandler} required />
                     <input name="micronId" placeholder="Micron ID" value={userForm.micronId} onChange={changeHandler} required />
+                    <input name="micronUser" placeholder="Micron User" value={userForm.micronUser} onChange={changeHandler} required />                    
                     <input name="micronPass" placeholder="Micron Pass" value={userForm.micronPass} onChange={changeHandler} required />
-                    <SelectTenantOptions onSelectTenant={changeHandler}/>
-                    <br/>
-                    <SelectClubOptions onSelectClub={changeHandler}/>
+                    <p>Select user rol:</p>
+                    <br />
+                    <label htmlFor="type1">Tenant admin</label>
+                    <input
+                        className="rdbttn"
+                        type="radio"
+                        id="type1"
+                        name="userType"
+                        value="2"
+                        checked={userForm.userType === 2}
+                        onChange={changeHandler}
+                    />
+                    <br />
+                    <label htmlFor="type2">Club admin</label>
+                    <input
+                        className="rdbttn"
+                        type="radio"
+                        id="type2"
+                        name="userType"
+                        value="1"
+                        checked={userForm.userType === 1}
+                        onChange={changeHandler}
+                    />
+                    <br />
+                    <SelectTenantOptions onSelectTenant={changeHandler} />
+                    <br />
+                    <SelectClubOptions onSelectClub={changeHandler} />
                     <br />
                     <button type="submit">Registrar</button>
                     <button type="button" onClick={onClose}>Cancelar</button>
