abeacb01bad2c78ff5e4d4b65579dce752b3a5be
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-29 09:03:24 +0200

    Feat(components): add a default option in the tenant select

--- src/presentation/components/SelectTenantOptions/SelectTenantOptions.tsx
+++ src/presentation/components/SelectTenantOptions/SelectTenantOptions.tsx
@@ -30,6 +30,7 @@ function SelectTenantOptions({onSelectTenant}: SelectTenantOptionProps) {
             <label htmlFor="tenants">Choose a tenant:</label>
             <br/>
             <select name="tenantId" id="tenants" onClick={onClickHandler} onBlur={()=> setSelectOpen(false)} onChange={onSelectTenant}>
+                <option>----- -----</option>
                 {
                     potentialTenants.map(
                         (t) => <option key={t.tenantId} value={t.tenantId}>{t.name}</option>
