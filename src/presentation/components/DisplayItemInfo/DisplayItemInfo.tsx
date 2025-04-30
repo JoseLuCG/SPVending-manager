dc6c5756e334c91bfe235de322491ef51b9f9e2d
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-30 10:26:30 +0200

    Feat(components): create the `DisplayItemInfo` component

---
+++ src/presentation/components/DisplayItemInfo/DisplayItemInfo.tsx
@@ -0,0 +1,16 @@
+import "./DisplayItemInfo.css"
+
+function DisplayItemInfo() {
+    
+
+	return (
+        <>
+            <main>
+                <h1>Hola mundo</h1>
+                <p>Esta pagina es de prueba</p>
+            </main>    
+        </>
+	)
+}
+
+export default DisplayItemInfo;
