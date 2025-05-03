6b79bc54f6b8d80f215fa3e955f3d27ba3de3531
JoseLuCG <joseluisconde@protonmail.com>
2025-05-03 14:32:40 +0200

    Fix(tools): add missing return statements in `getEntityId` function

--- src/utilities/tools/checkers.ts
+++ src/utilities/tools/checkers.ts
@@ -3,6 +3,6 @@ import { DataTableValue } from "primereact/datatable";
 export function getEntityId(rowData:DataTableValue) {
     if ("tenantId" in rowData) return rowData.tenantId;
     if ("clubId" in rowData) return rowData.clubId;
-    if ("userManagerId" in rowData) rowData.userManagerId;
-    if ("machineId" in rowData) rowData.machineId;
+    if ("userManagerId" in rowData) return rowData.userManagerId;
+    if ("machineId" in rowData) return rowData.machineId;
 }
