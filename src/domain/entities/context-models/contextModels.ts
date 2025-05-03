495453f91606e6ce669c49c4723f7faecc20a8f7
JoseLuCG <joseluisconde@protonmail.com>
2025-05-03 22:07:37 +0200

    Refactor(context-models): unify `SelectedItemType` to consistently include null

--- src/domain/entities/context-models/contextModels.ts
+++ src/domain/entities/context-models/contextModels.ts
@@ -4,9 +4,9 @@ import { Machine } from "../models/machine";
 import { Tenant } from "../models/tenant";
 import { User } from "../models/user";
 
-export type SelectedItemContextType = [SelectedItemType|null, React.Dispatch<React.SetStateAction<SelectedItemType|null>>];
+export type SelectedItemContextType = [SelectedItemType, React.Dispatch<React.SetStateAction<SelectedItemType|null>>];
 
-export type SelectedItemType = User | Machine | Tenant | Club;
+export type SelectedItemType = User | Machine | Tenant | Club | null;
 
 export type SelectedItemProviderProps = {
     children: ReactNode;
