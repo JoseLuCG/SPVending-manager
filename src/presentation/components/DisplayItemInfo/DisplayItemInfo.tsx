f23ee3733b3d42b3dbf3d34020e2a0cf2be8d4f9
JoseLuCG <joseluisconde@protonmail.com>
2025-05-05 08:39:07 +0200

    Feat(DisplayItemInfo): implement context support and `typeDeterminer` utility

--- src/presentation/components/DisplayItemInfo/DisplayItemInfo.tsx
+++ src/presentation/components/DisplayItemInfo/DisplayItemInfo.tsx
@@ -1,13 +1,31 @@
 import "./DisplayItemInfo.css"
 import { useNavigate } from "react-router";
 import DisplayItemInfoTenant from "./DisplayItemInfoTenant/DisplayItemInfoTenant";
+import { useContext, useEffect, useState } from "react";
+import { SelectedItem } from "../../../contexts/SelectedItemContext";
+import { typeDeterminer } from "../../../utilities/tools/checkers";
 
 function DisplayItemInfo() {
+    // States:
+    const [ item, setItem ] = useContext(SelectedItem);
     const navigate = useNavigate();
+    let itemType = "";
 
+    // Handlers:
     function backHandler() {
+        setItem(null);
         navigate(-1);
     }
+
+    // Functions:
+
+
+    useEffect(()=>{
+        if (item != null) {
+            itemType = typeDeterminer(item);
+            console.log(itemType);
+        }
+    });
 
 	return (
         <>
