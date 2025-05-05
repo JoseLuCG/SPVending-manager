9afe48e9e3a76eca960303023b80e438b5a70ffa
Jose Luis <condegomesjoseluis@gmail.com>
2025-05-05 13:32:31 +0200

    Feat(Header): update `Header` component to support css modules

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import './Header.css';
+import styles from './Header.module.css';
 import userIcon from './../../../assets/icons/UserIcon.svg';
 import { useState } from 'react';
 
@@ -6,11 +6,11 @@ function Header() {
     const [ admin, /*setAdmin*/ ] = useState("User Name");
 
     return(
-        <header>
+        <header className={styles.header}>
             <h1>SPVending Manager</h1>
             <div>
                 <h2>{admin}</h2>
-                <div className='picContainer'>
+                <div className={styles.picContainer}>
                     <img src={userIcon} alt="profile pic" height="60" width="60"/>
                 </div>
             </div>
