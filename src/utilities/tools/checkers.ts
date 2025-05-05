8cbaccb1f2811bf086d85c503e97897204a51892
JoseLuCG <joseluisconde@protonmail.com>
2025-05-05 08:29:12 +0200

    Feat(utilities): add `typeDeterminer` utility for identifying the type of the param

--- src/utilities/tools/checkers.ts
+++ src/utilities/tools/checkers.ts
@@ -1,8 +1,18 @@
 import { DataTableValue } from "primereact/datatable";
+import { SelectedItemType } from "../../domain/entities/context-models/contextModels";
 
 export function getEntityId(rowData:DataTableValue) {
     if ("tenantId" in rowData) return rowData.tenantId;
     if ("clubId" in rowData) return rowData.clubId;
     if ("userManagerId" in rowData) return rowData.userManagerId;
     if ("machineId" in rowData) return rowData.machineId;
+}
+
+export function typeDeterminer(item:SelectedItemType): string {
+    if (item === null) return "Error";
+    if ("tenantId" in item) return "Tenant";
+    if ("clubId" in item) return "Club";
+    if ("userManagerId" in item) return "User";
+    if ("machineId" in item) return "Machine";
+    return "Unknow";
 }
