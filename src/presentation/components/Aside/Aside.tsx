433773a54ec0cb92af00e8283b3459a2938b343e
Jose Luis <condegomesjoseluis@gmail.com>
2025-05-05 13:00:53 +0200

    Feat(Aside): update `Aside` component to support css modules

--- src/presentation/components/Aside/Aside.tsx
+++ src/presentation/components/Aside/Aside.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import MenuButton from '../MenuButton/MenuButton';
 import NavButtons from './components/NavButtons/NavButtons';
-import './Aside.css';
+import styles from './Aside.module.css';
 
 function Aside() {
     const [ isAbove, setIsAbove ] = useState(false);
 
     return(
         <aside
+        className={styles.aside}
             onMouseEnter={() => setIsAbove(true)}
             onMouseLeave={() => setIsAbove(false)}
         >
