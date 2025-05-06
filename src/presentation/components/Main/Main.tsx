af05a91f3cb99f7be9f03f016a239e1dce184f29
Jose Luis <condegomesjoseluis@gmail.com>
2025-05-06 10:19:50 +0200

    Feat(Main): add navigation support with `useNavigate()` and clear the code
    Cleared the Main component of unnecessary code and add
    navigation support through `useNavigate()` hook.

--- src/presentation/components/Main/Main.tsx
+++ src/presentation/components/Main/Main.tsx
@@ -4,77 +4,30 @@ import Aside from './../Aside/Aside';
 import { DataTable, DataTableRowClickEvent } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { MainProps } from './../../../domain/entities/property-models/componentsProperties';
-import { useContext, useEffect, useState } from 'react';
+import { useState } from 'react';
 import Modal from '../Modals/Modal';
-import { SelectedItem } from '../../../contexts/SelectedItemContext';
 import { getEntityId } from '../../../utilities/tools/checkers';
-import { TenantRepositoryHttp } from '../../../infraestructure/adapters/api/TenantRepositoryHttp';
-import { ClubRepositoryHttp } from '../../../infraestructure/adapters/api/ClubRepositoryHttp';
-import { UserRepositoryHttp } from '../../../infraestructure/adapters/api/UserRepositoryHttp';
-import { MachineRepositoryHttp } from '../../../infraestructure/adapters/api/MachineRepositoryHttp';
-import { GetTenant } from '../../../application/usecases/TenantUseCases/GetTenant';
-import { GetClub } from '../../../application/usecases/ClubUseCases/GetClub';
-import { GetMachine } from '../../../application/usecases/MachineUseCases/GetMachine';
-import { GetUser } from '../../../application/usecases/UserUseCases/GetUser';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import { appRoutes } from '../../../utilities/defines/routes';
-
-const tenantRepository = new TenantRepositoryHttp();
-const clubRepository = new ClubRepositoryHttp();
-const userRepository = new UserRepositoryHttp();
-const machineRepository = new MachineRepositoryHttp();
 
 function Main({textInfoDisplay, dataToDisplay}: MainProps) {
 	// ---------- States ----------
-	const [ item, setItem ] = useContext(SelectedItem);
+	//const [ item, setItem ] = useContext(SelectedItem);
 	const [ showModal, setShowModal ] = useState(false);
 	const [ rowSelected ] = useState({});
 	const navigate = useNavigate();
+	const location = useLocation();
 
 	// Handlers:
 	function selectionRowHandler(event:DataTableRowClickEvent) {
 		const selectedItemId = getEntityId(event.data);
-
-		if (textInfoDisplay.list === "Tenant") {
-			const findTenantById = new GetTenant(tenantRepository);
-			findTenantById.execute(selectedItemId)
-				.then(setItem)
-				.catch(console.error);
-		}
-
-		if (textInfoDisplay.list === "Club") {
-			const findClubById = new GetClub(clubRepository);
-			findClubById.execute(selectedItemId)				
-				.then(setItem)
-				.catch(console.error);
-			
-		}
-
-		if (textInfoDisplay.list === "Machine") {
-			const findMachineById = new GetMachine(machineRepository);
-			findMachineById.execute(selectedItemId)
-				.then(setItem)
-				.catch(console.error);
-		}
-
-		if (textInfoDisplay.list === "User") {
-			const findUserById = new GetUser(userRepository);
-			findUserById.execute(selectedItemId)
-				.then(setItem)
-				.catch(console.error);
-		}
+		navigate(`${location.pathname}` +  appRoutes.selectedItemRoute + `/${selectedItemId}`, {state:{from:location.pathname}});
 	}
 
 	// ---------- Styles ----------
 	const rowClassName = () => {
 		return  `${styles.tableRow} ${styles.pDatatableThead}`;
 	};
-
-	useEffect(()=>{
-		if (item != null) {
-			navigate(appRoutes.selectedItemRoute);
-		}
-	});
 
 	return (
 		<main>
