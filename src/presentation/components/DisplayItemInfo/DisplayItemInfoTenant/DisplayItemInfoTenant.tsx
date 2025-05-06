2d8c311b58b6a364676ea378681f1f36aa5d394f
Jose Luis <condegomesjoseluis@gmail.com>
2025-05-06 13:57:33 +0200

    Feat(DisplayItemInfo): add a button that allows editong and add the disabled attribute in the tags

--- src/presentation/components/DisplayItemInfo/DisplayItemInfoTenant/DisplayItemInfoTenant.tsx
+++ src/presentation/components/DisplayItemInfo/DisplayItemInfoTenant/DisplayItemInfoTenant.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { DIITenantProps } from "../../../../domain/entities/property-models/componentsProperties";
 import "./../DisplayItemInfo.css";
 
-function DisplayItemInfoTenant({object}:DIITenantProps) {
+function DisplayItemInfoTenant({ object }: DIITenantProps) {
+    // States:
+    const [ isDisabled, setIsDisabled ] = useState(true); 
+
+    // Handlers:
+    function onClickHandler(){
+        setIsDisabled(previous => !previous);
+    }
+
 
     if (!object) {
-        return(
+        return (
             <div>
                 <p>Loading...</p>
             </div>
@@ -12,60 +21,73 @@ function DisplayItemInfoTenant({object}:DIITenantProps) {
     }
 
     return (
-        <form className="frm-cntnr">
-            <div className="frm-dv">
-                <label htmlFor="tenantName">Tenant Name</label>
-                <input 
-                    id="tenantName" 
-                    type="text" 
-                    value={object.name} 
-                />
-                <label htmlFor="address">Tenant Address</label>
-                <input 
-                    id="address" 
-                    type="text" 
-                    value={object.address}    
-                />
-                <label htmlFor="email">Tenant email</label>
-                <input 
-                    id="email" 
-                    type="text"
-                    value={object.email} 
-                />
-                <label htmlFor="remark">Remark</label>
-                <input 
-                    id="remark" 
-                    type="text"
-                    value={object.remark} 
-                />
+        <>
+            <div>
+                <button onClick={onClickHandler}>Eddit</button>
             </div>
-            <div className="frm-dv">
-                <label htmlFor="tenantCIF">CIF</label>
-                <input 
-                    id="tenantCIF" 
-                    type="text" 
-                    value={object.cif}
-                />
-                <label htmlFor="phone">Phone</label>
-                <input 
-                    id="phone" 
-                    type="text"
-                    value={object.phone}
-                />
-                <label htmlFor="micronId">Micron ID</label>
-                <input 
-                    id="micronId" 
-                    type="text"
-                    value={object.micronId}
-                />
-                <label htmlFor="managers">Managers</label>
-                <input 
-                    id="managers" 
-                    type="text"
-                    value={object.managers}
-                />
-            </div>
-        </form>
+            <form className="frm-cntnr">
+                <div className="frm-dv">
+                    <label htmlFor="tenantName">Tenant Name</label>
+                    <input
+                        id="tenantName"
+                        type="text"
+                        value={object.name}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="address">Tenant Address</label>
+                    <input
+                        id="address"
+                        type="text"
+                        value={object.address}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="email">Tenant email</label>
+                    <input
+                        id="email"
+                        type="text"
+                        value={object.email}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="remark">Remark</label>
+                    <input
+                        id="remark"
+                        type="text"
+                        value={object.remark}
+                        disabled={isDisabled}
+                    />
+                </div>
+                <div className="frm-dv">
+                    <label htmlFor="tenantCIF">CIF</label>
+                    <input
+                        id="tenantCIF"
+                        type="text"
+                        value={object.cif}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                        id="phone"
+                        type="text"
+                        value={object.phone}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="micronId">Micron ID</label>
+                    <input
+                        id="micronId"
+                        type="text"
+                        value={object.micronId}
+                        disabled={isDisabled}
+                    />
+                    <label htmlFor="managers">Managers</label>
+                    <input
+                        id="managers"
+                        type="text"
+                        value={object.managers}
+                        disabled={isDisabled}
+                    />
+                </div>
+            </form>
+        </>
     );
 }
 
