9396ee4ef274e88560c185bbc1665f204387ebcb
Jose Luis <condegomesjoseluis@gmail.com>
2025-05-06 10:17:22 +0200

    Refactor(src): remove unnecessary types and functions

--- src/utilities/tools/checkers.ts
+++ src/utilities/tools/checkers.ts
@@ -7,12 +7,3 @@ export function getEntityId(rowData:DataTableValue) {
     if ("userManagerId" in rowData) return rowData.userManagerId;
     if ("machineId" in rowData) return rowData.machineId;
 }
-
-export function typeDeterminer(item:SelectedItemType): string {
-    if (item === null) return "Error";
-    if ("tenantId" in item) return "Tenant";
-    if ("clubId" in item) return "Club";
-    if ("userManagerId" in item) return "User";
-    if ("machineId" in item) return "Machine";
-    return "Unknow";
-}
