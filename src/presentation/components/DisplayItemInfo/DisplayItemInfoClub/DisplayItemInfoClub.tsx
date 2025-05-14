88be249f81d534e297bf673e69d89551d38f7528
JoseLuCG <joseluisconde@protonmail.com>
2025-05-14 09:34:58 +0200

    Styles(DisplayItemInfoClub): update the styles of DisplayItemInfoClub

--- src/presentation/components/DisplayItemInfo/DisplayItemInfoClub/DisplayItemInfoClub.tsx
+++ src/presentation/components/DisplayItemInfo/DisplayItemInfoClub/DisplayItemInfoClub.tsx
@@ -38,7 +38,7 @@ function DisplayItemInfoClub({ object }: DIIClubProps) {
                 micronId: item.micronId,
                 accountingId: item.accountingId,
                 tenantId: item.tenantEntityName,
-                clubId:item.clubId
+                clubId: item.clubId
             }
             return dataMapped;
         }
@@ -50,7 +50,7 @@ function DisplayItemInfoClub({ object }: DIIClubProps) {
         setIsDisabled(previous => !previous);
     }
 
-    function changeHandler(event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) {
+    function changeHandler(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
         const { name, value } = event.target;
         setClubFormData(prev => ({
             ...prev,
@@ -90,108 +90,130 @@ function DisplayItemInfoClub({ object }: DIIClubProps) {
                 <button onClick={onClickHandler}>Eddit</button>
             </div>
             <form className="frm-cntnr" onSubmit={submitHandler}>
-                <div className="frm-dv">
-                    <label htmlFor="clubName">Club Name</label>
-                    <input
-                        id="clubName"
-                        name="clubName"
-                        type="text"
-                        value={isDisabled? object.name: clubFormData.clubName}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.name}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="tenantEntity">Tenant Owner</label>
-                    {
-                        isDisabled?
+                <main className="frm-mc-cntnr">
+                    <div className="frm-dv">
+                        <div className="fp-div">
+                            <label htmlFor="clubName">Club Name</label>
                             <input
-                                id="tenantEntity"
-                                name="tenantId"
+                                id="clubName"
+                                name="clubName"
                                 type="text"
-                                value={object.tenantEntityName}
-                            />:
-                            <SelectTenantOptions onSelectTenant={changeHandler}/>
-                    }
-                    <label htmlFor="address">Club Address</label>
-                    <input
-                        id="address"
-                        name="address"
-                        type="text"
-                        value={isDisabled?object.address:clubFormData.address}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.address}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="email">Club email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="text"
-                        value={isDisabled?object.email:clubFormData.email}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.email}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="remark">Remark</label>
-                    <input
-                        id="remark"
-                        name="remark"
-                        type="text"
-                        value={isDisabled?object.remark:clubFormData.remark}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.remark}
-                        onChange={changeHandler}
-                    />
-                </div>
-                <div className="frm-dv">
-                    <label htmlFor="clubCIF">CIF</label>
-                    <input
-                        id="clubCIF"
-                        name="cif"
-                        type="text"
-                        value={isDisabled?object.cif:clubFormData.cif}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.cif}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="accountingId">Accounting ID</label>
-                    <input
-                        id="accountingId"
-                        name="accountingId"
-                        type="text"
-                        value={isDisabled?object.accountingId:clubFormData.accountingId}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.accountingId}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                        id="phone"
-                        name="phone"
-                        type="text"
-                        value={isDisabled?object.phone:clubFormData.phone}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.phone}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="micronId">Micron ID</label>
-                    <input
-                        id="micronId"
-                        name="micronId"
-                        type="text"
-                        value={isDisabled?object.micronId:clubFormData.micronId}
-                        disabled={isDisabled}
-                        placeholder={isDisabled?"":object.micronId}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="managers">Managers</label>
-                    <input
-                        id="managers"
-                        type="text"
-                        value={object.userManagers}
-                        disabled
-                    />
-                </div>
+                                value={isDisabled ? object.name : clubFormData.clubName}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.name}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="tenantEntity">Tenant Owner</label>
+                            {
+                                isDisabled ?
+                                    <input
+                                        id="tenantEntity"
+                                        name="tenantId"
+                                        type="text"
+                                        value={object.tenantEntityName}
+                                    /> :
+                                    <SelectTenantOptions onSelectTenant={changeHandler} />
+                            }
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="address">Club Address</label>
+                            <input
+                                id="address"
+                                name="address"
+                                type="text"
+                                value={isDisabled ? object.address : clubFormData.address}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.address}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="email">Club email</label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="text"
+                                value={isDisabled ? object.email : clubFormData.email}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.email}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="remark">Remark</label>
+                            <input
+                                id="remark"
+                                name="remark"
+                                type="text"
+                                value={isDisabled ? object.remark : clubFormData.remark}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.remark}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                    </div>
+                    <div className="frm-dv">
+                        <div className="fp-div">
+                            <label htmlFor="clubCIF">CIF</label>
+                            <input
+                                id="clubCIF"
+                                name="cif"
+                                type="text"
+                                value={isDisabled ? object.cif : clubFormData.cif}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.cif}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="accountingId">Accounting ID</label>
+                            <input
+                                id="accountingId"
+                                name="accountingId"
+                                type="text"
+                                value={isDisabled ? object.accountingId : clubFormData.accountingId}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.accountingId}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="phone">Phone</label>
+                            <input
+                                id="phone"
+                                name="phone"
+                                type="text"
+                                value={isDisabled ? object.phone : clubFormData.phone}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.phone}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="micronId">Micron ID</label>
+                            <input
+                                id="micronId"
+                                name="micronId"
+                                type="text"
+                                value={isDisabled ? object.micronId : clubFormData.micronId}
+                                disabled={isDisabled}
+                                placeholder={isDisabled ? "" : object.micronId}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                        <div className="fp-div">
+                            <label htmlFor="managers">Managers</label>
+                            <input
+                                id="managers"
+                                type="text"
+                                value={object.userManagers}
+                                disabled
+                            />
+                        </div>
+                    </div>
+                </main>
                 <div>
                     <button type="submit">Submit</button>
                 </div>
