0389d1adf4cfd10f5a1e3f120c7dd553b0fe73d7
JoseLuCG <joseluisconde@protonmail.com>
2025-05-17 13:06:13 +0200

    Feat(App): add the `LogginPage` component to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import MachinesPage from './presentation/pages/MachinesPage/MachinesPage';
 import UsersPage from './presentation/pages/UsersPage/UsersPage';
 import { appRoutes } from './utilities/defines/routes';
 import SelectedItemPage from './presentation/pages/SelectedItemPage/SelectedItemPage';
+import LogginPage from './presentation/pages/LogginPage/LogginPage';
 
 function App() {
 
 	return (
 		<>
 			<Routes>
+				<Route path={appRoutes.logginRoute} element={<LogginPage/>}/>
 				<Route path={appRoutes.tenantsRoute} element={<TenantsPage/>}/>
 				<Route path={appRoutes.clubsRoute} element={<ClubsPage/>}/>
 				<Route path={appRoutes.machinesRoute} element={<MachinesPage/>}/>
