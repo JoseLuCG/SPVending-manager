286c4796c135ba225e1c9197bf538039236d56a4
JoseLuCG <joseluisconde@protonmail.com>
2025-05-19 01:19:34 +0200

    Styles(Main): modify the main component upadting the styles and reorganizing the tag elements

--- src/presentation/components/Main/Main.tsx
+++ src/presentation/components/Main/Main.tsx
@@ -11,61 +11,63 @@ import { getEntityId } from '../../../utilities/tools/checkers';
 import { useLocation, useNavigate } from 'react-router';
 import { appRoutes } from '../../../utilities/defines/routes';
 
-function Main({textInfoDisplay, dataToDisplay, setterUuid}: MainProps) {
+function Main({ textInfoDisplay, dataToDisplay, setterUuid }: MainProps) {
 	// ---------- States ----------
-	const [ showModal, setShowModal ] = useState(false);
-	const [ rowSelected ] = useState({});
+	const [showModal, setShowModal] = useState(false);
+	const [rowSelected] = useState({});
 	const navigate = useNavigate();
 	const location = useLocation();
 
 	// Handlers:
-	function selectionRowHandler(event:DataTableRowClickEvent) {
+	function selectionRowHandler(event: DataTableRowClickEvent) {
 		const selectedItemId = getEntityId(event.data);
-		navigate(`${location.pathname}` +  appRoutes.selectedItemRoute + `/${selectedItemId}`, {state:{from:location.pathname}});
+		navigate(`${location.pathname}` + appRoutes.selectedItemRoute + `/${selectedItemId}`, { state: { from: location.pathname } });
 	}
 
-	function onClickHandler(data:any) {
+	function onClickHandler(data: any) {
 		const dataId = getEntityId(data);
 		setterUuid(dataId);
 	}
 	// ---------- Styles ----------
 	const rowClassName = () => {
-		return  `${styles.tableRow} ${styles.pDatatableThead}`;
+		return `${styles.tableRow} ${styles.pDatatableThead}`;
 	};
 
 	return (
 		<main>
 			<Aside />
-			<section>
-				<div className={styles.tableContents}>
+			<section className={styles.sectionContainer}>
+				<div className={styles.container}>
 					<div className={styles.headerContent}>
 						<p className={styles.infText}>{textInfoDisplay.list} List</p>
-						<button className={styles.addButton} onClick={() => setShowModal(true) }>+ Add {textInfoDisplay.list}</button>
-						<Modal typeModal={textInfoDisplay.list} isOpen={showModal} onClose={() => setShowModal(false)}/>
 						<input className={styles.searcher} type="text" placeholder='Buscar...' />
+						<Modal typeModal={textInfoDisplay.list} isOpen={showModal} onClose={() => setShowModal(false)} />
+						<button className={styles.addButton} onClick={() => setShowModal(true)}>+ Add {textInfoDisplay.list}</button>
 					</div>
-					<DataTable 
-						value={dataToDisplay} 
-						selectionMode="single" 
-						rowClassName={rowClassName} 
-						selection={rowSelected}
-						onRowClick={selectionRowHandler}
-					>
-						<Column headerClassName={styles.headerTB} field={textInfoDisplay.column1.field} header={textInfoDisplay.column1.header}></Column>
-						<Column headerClassName={styles.headerTB} field={textInfoDisplay.column2.field} header={textInfoDisplay.column2.header}></Column>
-						<Column headerClassName={styles.headerTB} field={textInfoDisplay.column3.field} header={textInfoDisplay.column3.header}></Column>
-						<Column headerClassName={styles.headerTB} field={textInfoDisplay.column4.field} header={textInfoDisplay.column4.header}></Column>
-						<Column headerClassName={styles.headerTB} field={textInfoDisplay.column5.field} header={textInfoDisplay.column5.header}></Column>
-						<Column headerClassName={styles.headerTB} field="" header="Actions"
-							body={(rowData) => (
-								<div>
-									<button onClick={()=> onClickHandler(rowData)}>
-										<img src={trashIcon} alt="" />
-									</button>
-								</div>
-							)}
-						/>
-					</DataTable>
+					<div className={styles.tableContainer}>
+						<DataTable
+							value={dataToDisplay}
+							selectionMode="single"
+							rowClassName={rowClassName}
+							selection={rowSelected}
+							onRowClick={selectionRowHandler}
+						>
+							<Column headerClassName={styles.headerTB} field={textInfoDisplay.column1.field} header={textInfoDisplay.column1.header}></Column>
+							<Column headerClassName={styles.headerTB} field={textInfoDisplay.column2.field} header={textInfoDisplay.column2.header}></Column>
+							<Column headerClassName={styles.headerTB} field={textInfoDisplay.column3.field} header={textInfoDisplay.column3.header}></Column>
+							<Column headerClassName={styles.headerTB} field={textInfoDisplay.column4.field} header={textInfoDisplay.column4.header}></Column>
+							<Column headerClassName={styles.headerTB} field={textInfoDisplay.column5.field} header={textInfoDisplay.column5.header}></Column>
+							<Column headerClassName={styles.headerTB} field="" header="Actions"
+								body={(rowData) => (
+									<div>
+										<button onClick={() => onClickHandler(rowData)}>
+											<img src={trashIcon} alt="" />
+										</button>
+									</div>
+								)}
+							/>
+						</DataTable>
+					</div>
 				</div>
 			</section>
 		</main>
