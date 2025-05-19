7ed2543bd9ff38bc59e53e516a4f694c05f110d6
JoseLuCG <joseluisconde@protonmail.com>
2025-05-19 03:07:08 +0200

    Feat(Header): allow show the admin name in the header component

--- src/presentation/components/Header/Header.tsx
+++ src/presentation/components/Header/Header.tsx
@@ -1,15 +1,33 @@
 import styles from './Header.module.css';
 import userIcon from './../../../assets/icons/UserIcon.svg';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { Admin } from '../../../contexts/AdminContext';
 
 function Header() {
-    const [ admin, /*setAdmin*/ ] = useState("User Name");
+    const [admin, setAdmin] = useContext(Admin);
+    const [ adminUser, setAdminUser ] = useState("User Name");
+
+    useEffect(
+        ()=> {
+            console.log(admin);
+            
+        }
+    );
+
+    useEffect(
+        () => {
+            if (admin != null) {
+                const user:string = admin.name
+                setAdminUser(user);
+            }
+        }, [admin]
+    );
 
     return(
         <header className={styles.header}>
             <h1>SPVending Manager</h1>
             <div>
-                <h2>{admin}</h2>
+                <h2>{adminUser}</h2>
                 <div className={styles.picContainer}>
                     <img src={userIcon} alt="profile pic" height="60" width="60"/>
                 </div>
