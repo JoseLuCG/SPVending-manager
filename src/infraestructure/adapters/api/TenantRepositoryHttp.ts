721a075f0130471a8d0802ee198422fd8661315d
JoseLuCG <joseluisconde@protonmail.com>
2025-05-20 10:20:03 +0200

    [WORK IN PROGRESS!]: add commented code for the `authHandler()` implementation

--- src/infraestructure/adapters/api/TenantRepositoryHttp.ts
+++ src/infraestructure/adapters/api/TenantRepositoryHttp.ts
@@ -3,6 +3,7 @@ import { PotentialTenant, Tenant, TenantApi, TenantInfoDisplay } from "../../../
 import { BASE_URL_SERVER, API_PREFIX, PATH_PREFIX } from "../../../utilities/defines/api/api-routes";
 import { mapTenantFromApi } from "../../mappers/FromApi/TenantMapper";
 import { mapTenantToApi } from "../../mappers/ToApi/TenantMapperToApi";
+import { authHandler } from "../Auth/AuthHandler";
 
 export class TenantRepositoryHttp implements TenantRepository {
     private BASEURL = BASE_URL_SERVER + API_PREFIX + PATH_PREFIX.tenantPath;
@@ -15,7 +16,14 @@ export class TenantRepositoryHttp implements TenantRepository {
 
     async getAllTenants(): Promise<TenantInfoDisplay[]> {
         const response = await fetch(this.BASEURL, {credentials: "include"});
+        //const authorizedResponse = authHandler(response);
         const json = await response.json();
+        /*if (response.status == 401) {
+            const refreshedJson = authHandler(response);
+            
+            return mapTenantFromApi(refreshedJson);
+        }*/
+       
         return mapTenantFromApi(json);
     }
 
