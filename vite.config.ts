4234b5e976caf619638e6bb368c5a06da782695a
JoseLuCG <joseluisconde@protonmail.com>
2025-05-20 13:02:40 +0200

    Fix(vite.config): update the target URL of the vite proxy to connect to the correct direction

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://spvending-api-container:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
