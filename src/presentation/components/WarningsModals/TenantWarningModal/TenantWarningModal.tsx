5c7a93e88a4a315d93c5f6d6dc22f41bd5539fda
JoseLuCG <joseluisconde@protonmail.com>
2025-05-27 11:41:34 +0200

    Refactor(Warning modals): change the old warning modal to a Dialog component

--- src/presentation/components/WarningsModals/TenantWarningModal/TenantWarningModal.tsx
+++ src/presentation/components/WarningsModals/TenantWarningModal/TenantWarningModal.tsx
@@ -1,46 +1,54 @@
-import styles from "./../WarningsModals.module.css";
+import { useState } from "react";
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import styles from "./../WarningsModals.module.css"
+import alertIcon from "./../../../../assets/icons/alert.png";
 import { WarningProps } from "../../../../domain/entities/property-models/componentsProperties";
 import { TenantRepositoryHttp } from "../../../../infraestructure/adapters/api/TenantRepositoryHttp";
 import { DeleteTenant } from "../../../../application/usecases/TenantUseCases/DeleteTenant";
-import alertIcon from "./../../../../assets/icons/alert.png";
 
 const repository = new TenantRepositoryHttp();
 const deleteTenant = new DeleteTenant(repository);
 
-function TenantWarningModal({ isOpen, onClose, uuid, setUuid }: WarningProps) {
-   
-    function onClickHandler() {
+function TenantWarningModal({visible, setVisible, uuid, setUuid, position}: WarningProps) {
+    // Handlers:
+    function closeWindow(){
+        setUuid("");
+        setVisible();
+    }
+
+    function onDeleteHandler() {
         if (uuid != "") {
             console.log(uuid);
-			deleteTenant.execute(uuid);
-			setUuid("");
-            onClose();
+            deleteTenant.execute(uuid);
+            setUuid("");
+            setVisible();
             window.location.reload();
-		}
+        }
     }
-
-    function onCloseHandler() {
-        setUuid("");
-        onClose();
-    }
-
-    if (!isOpen) return null;
-    return(
-        <>
-            <div className={styles.modalOverlay}>
-            <div className={styles.modal}>
-                <div className={styles.content}>
-                    <img className={styles.imgAlert} src={alertIcon} alt="" height="200" width="200"/>
-                    <h2 className={styles.textAlert}>Are you sure you want to delete the Tenant?</h2>
-                </div>
-                <div className={styles.buttonsContainer}>
-                    <button className={styles.button} type="button" onClick={onClickHandler}>Delete</button>
-                    <button className={styles.button} type="button" onClick={onCloseHandler}>Cancel</button>
-                </div>
-            </div>
+    
+    const footerContent = (
+        <div>
+            <Button label="No" icon="pi pi-times" onClick={closeWindow} className={`p-button-text ${styles.btnPddng}`} />
+            <Button label="Yes" icon="pi pi-check" onClick={onDeleteHandler} className={styles.btnPddng} autoFocus />
         </div>
-        </>
     );
+    const header = (
+        <div>
+            <img src={alertIcon} alt="" width="30px" height="30px"/>
+            <h2>Deleting tenant:</h2>
+        </div>
+    );
+
+    return (
+        <div className="card px-2">
+            <Dialog header={header} visible={visible} position={position} style={{ width: '50vw', padding:20, background:'#ffffff' }} onHide={() => {if (!visible) return; setVisible }} footer={footerContent} draggable={false} resizable={false}>
+                <p className="m-0">
+                    Are you sure you want to delete this tenant?
+                </p>
+            </Dialog>
+        </div>
+    )
 }
 
 export default TenantWarningModal;
