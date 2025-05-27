64107ca8d878019c2fd6bd78eacccd183b7a3a5d
JoseLuCG <joseluisconde@protonmail.com>
2025-05-27 11:42:36 +0200

    Feat(TenantPage): implement the dialog in the tenant page

--- src/presentation/pages/TenantsPage/TenantsPage.tsx
+++ src/presentation/pages/TenantsPage/TenantsPage.tsx
@@ -7,36 +7,45 @@ import { TenantInfoDisplay } from '../../../domain/entities/models/tenant';
 import { TenantRepositoryHttp } from '../../../infraestructure/adapters/api/TenantRepositoryHttp';
 import { GetTenantList } from '../../../application/usecases/TenantUseCases/GetTenantList';
 import TenantWarningModal from '../../components/WarningsModals/TenantWarningModal/TenantWarningModal';
+import { Button } from 'primereact/button';
 
 const repository = new TenantRepositoryHttp();
 const getTenantList = new GetTenantList(repository);
 
 function TenantsPage() {
-	const [ tenants , setTenants ] = useState<TenantInfoDisplay[]>([]);
-	const [ uuid, setUuid ] = useState("");
-	const [ showModal, setShowModal ] = useState(false); 
-
-	useEffect(()=> {
+	const [tenants, setTenants] = useState<TenantInfoDisplay[]>([]);
+	const [uuid, setUuid] = useState("");
+	const [ visible, setVisible ] = useState<boolean>(false);
+	const [ position, setPosition ] = useState<string>("bottom");
+	const show = (position:string) => {
+		setPosition(position);
+		setVisible(true);
+	}
+	useEffect(() => {
 		getTenantList.execute()
 			.then(setTenants)
 			.catch(console.error);
 	}, []);
 
-	useEffect(()=> {
+	useEffect(() => {
 		if (uuid != "") {
-			setShowModal(true);
+			setVisible(true);
 		}
 	}, [uuid]);
 
 	return (
 		<>
 			<Header />
-			<Main textInfoDisplay={infoDisplayTenant} dataToDisplay={tenants} setterUuid={setUuid}/>
-			<TenantWarningModal 
-				isOpen={showModal} 
-				onClose={()=>{setShowModal(false)}} 
+			<Main textInfoDisplay={infoDisplayTenant} dataToDisplay={tenants} setterUuid={setUuid} />
+			<div className="flex flex-wrap justify-content-center gap-2">
+				<Button label="Bottom" icon="pi pi-arrow-up" onClick={() => show('bottom')} style={{ minWidth: '10rem' }} />
+			</div>
+			<TenantWarningModal
+				visible={visible}
+				setVisible={() => { setVisible(false) }}
 				uuid={uuid}
 				setUuid={setUuid}
+				position={position}
 			/>
 		</>
 	)
