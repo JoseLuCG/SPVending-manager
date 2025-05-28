78add4f2f050f92a20b7df8e1b8d1e8ac8dc39cf
JoseLuCG <joseluisconde@protonmail.com>
2025-05-28 21:08:20 +0200

    Feat(config): add commented code for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,32 @@
+// PRODUCTION
+/*
+import { defineConfig, loadEnv } from "vite";
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig(({ command, mode }) => {
+ const env = loadEnv(mode, process.cwd());
+ return {
+   plugins: [react()],
+   server: {
+     port: 5173,
+     host: true,
+     watch: {
+      usePolling: true,
+     },
+     esbuild: {
+      target: "esnext",
+      platform: "linux",
+    },
+  },
+  define: {
+    VITE_APP_BACKEND_ADDRESS: JSON.stringify(env.VITE_APP_BACKEND_ADDRESS),
+  },
+ };
+});
+*/
+
+// DEV
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
