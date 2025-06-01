773639e8817833ba999db161af9d8fcf1972cc2f
JoseLuCG <joseluisconde@protonmail.com>
2025-06-01 19:03:21 +0200

    configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 // PRODUCTION
 
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig/*, loadEnv*/ } from "vite";
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ /*command,*/ mode }) => {
- const env = loadEnv(mode, process.cwd());
+export default defineConfig((/*{ /*command, mode }*/) => {
+// const env = loadEnv(mode, process.cwd());
  return {
    plugins: [react()],
    server: {
@@ -17,10 +17,10 @@ export default defineConfig(({ /*command,*/ mode }) => {
       target: "esnext",
       platform: "linux",
     },
-  },
+  }/*,
   define: {
     VITE_APP_BACKEND_ADDRESS: JSON.stringify(env.VITE_APP_BACKEND_ADDRESS),
-  },
+  },*/
  };
 });
 
