9a2a960bf9a5e8cef28b12541eca66266ac5bc55
JoseLuCG <joseluisconde@protonmail.com>
2025-06-01 19:04:46 +0200

    change api-routes

--- src/utilities/defines/api/api-routes.ts
+++ src/utilities/defines/api/api-routes.ts
@@ -1,4 +1,5 @@
-export const BASE_URL_SERVER = "/api";
+/* http://172.18.0.6:8080 */
+export const BASE_URL_SERVER = `http://172.28.0.10:8080`;
 export const API_PREFIX = "/api/v1";
 export const PATH_PREFIX = {
     tenantPath: "/tenants",
