763931fc54e7c86e49f4313223c336f40b9c1eb1
JoseLuCG <joseluisconde@protonmail.com>
2025-06-02 09:57:23 +0200

    Docs(api-routes): add the actual ip for future static ip implementation

--- src/utilities/defines/api/api-routes.ts
+++ src/utilities/defines/api/api-routes.ts
@@ -1,4 +1,4 @@
-/* http://172.18.0.6:8080 */
+/* http://172.18.0.6:8080          http://172.28.0.4:3000/*/
 export const BASE_URL_SERVER = `http://172.28.0.10:8080`;
 export const API_PREFIX = "/api/v1";
 export const PATH_PREFIX = {
