2050c60d0324a83f69c5028a56907dcc01acdde2
JoseLuCG <joseluisconde@protonmail.com>
2025-06-02 09:53:08 +0200

    Fix(vite): change the port to expose the app

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig((/*{ /*command, mode }*/) => {
  return {
    plugins: [react()],
    server: {
-     port: 5173,
+     port: 3000,
      host: true,
      watch: {
       usePolling: true,
