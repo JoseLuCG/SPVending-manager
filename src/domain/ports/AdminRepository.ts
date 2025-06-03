832e9f85d8c72af33c52f5640aa4ea5eb2133a1b
JoseLuCG <joseluisconde@protonmail.com>
2025-06-03 10:24:27 +0200

    Feat(AdminRepository): add the `refreshAccessToken()` method

--- src/domain/ports/AdminRepository.ts
+++ src/domain/ports/AdminRepository.ts
@@ -4,4 +4,5 @@ import { Admin, AdminApi } from "../entities/models/admin";
 export interface AdminRepository {
     logAdmin(user:Omit<Admin,"id"|"roles">): Promise<AdminApi | string>;
     logOut(): Promise<boolean>;
+    refreshAccessToken(): Promise<AdminApi | null>;
 }
