ef279d17824e53ad24065a2bcaa1a5077602848a
JoseLuCG <joseluisconde@protonmail.com>
2025-06-04 09:41:56 +0200

    Fix(ClubCard): fix the `ClubCard` component

--- src/presentation/components/DisplayItemInfo/ClubCard/ClubCard.tsx
+++ src/presentation/components/DisplayItemInfo/ClubCard/ClubCard.tsx
@@ -1,14 +1,15 @@
+import styles from "./ClubCard.module.css"
 import { ClubCardProps } from "../../../../domain/entities/property-models/componentsProperties";
 
-function ClubCard({clubs}: ClubCardProps) {
-    return(
+function ClubCard({ club }: ClubCardProps) {
+    return (
         <div key={club.clubId} className={styles.clubCard}>
-        <h3>{club.name}</h3>
-        <p>{club.address}</p>
-        <p>{club.phone}</p>
-        <p>{club.machinesCount}</p>
-        <p>{club.userManagers}</p>                                    
-    </div>
+            <h3>{club.name}</h3>
+            <p>{club.address}</p>
+            <p>{club.phone}</p>
+            <p>{club.machinesCount}</p>
+            <p>{club.userManagers}</p>
+        </div>
     );
 }
 
