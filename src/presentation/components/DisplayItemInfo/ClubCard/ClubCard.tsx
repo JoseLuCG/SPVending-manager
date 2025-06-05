3ef1313c6c57ff78d9c7a2a83c8d1066013fa61a
JoseLuCG <joseluisconde@protonmail.com>
2025-06-05 11:31:07 +0200

    FEat(ClubCard): add styles of `DataTable` component and add the manager column

--- src/presentation/components/DisplayItemInfo/ClubCard/ClubCard.tsx
+++ src/presentation/components/DisplayItemInfo/ClubCard/ClubCard.tsx
@@ -4,15 +4,23 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 function ClubCard({ clubs }: ClubCardProps) {
+
+    // Styles
+    const rowClassName = () => {
+        return `${styles.tableRow}`;
+    }
     return (
         <div className={styles.section2}>
             <DataTable
                 value={clubs}
+                rowClassName={rowClassName}
+                className={styles.table}
             >
-                <Column field="name" header="Name"/>
-                <Column field="machinesCount" header="Machines"/>
-                <Column field="phone" header="Phone"/>
-                <Column field="address" header="Address"/>
+                <Column headerClassName={styles.headerTB} field="name" header="Name"/>
+                <Column headerClassName={styles.headerTB} field="machinesCount" header="Machines"/>
+                <Column headerClassName={styles.headerTB} field="phone" header="Phone"/>
+                <Column headerClassName={styles.headerTB} field="address" header="Address"/>
+                <Column headerClassName={styles.headerTB} field="userManagers" header="Managers"/>
             </DataTable>
         </div>
     );
