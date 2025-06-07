802bcdc3f565c37b8205ffd41ea2ed096e836a17
JoseLuCG <joseluisconde@protonmail.com>
2025-06-07 18:28:06 +0200

    Fix(UserMapperToApi): solve the problem when a user is modify having conflict with tenant and club uuids

--- src/infraestructure/mappers/ToApi/UserMapperToApi.ts
+++ src/infraestructure/mappers/ToApi/UserMapperToApi.ts
@@ -1,15 +1,29 @@
 import { User } from "../../../domain/entities/models/user";
 
 export function mapUserToApi(user: Partial<User>): any {
-    return {
-      username: user.username,
-      password: user.password,
-      micronId: user.micronId,
-      micronUser: user.micronUser,
-      micronPass: user.micronPass,
-      userType: user.userType,
-      tenantEntityId: user.tenantId,
-      clubEntityId: user.clubId,
-      userId: user.userId?user.userId:""
-    };
+	if (user.userType === 2) {
+		return {
+			username: user.username,
+			password: user.password,
+			micronId: user.micronId,
+			micronUser: user.micronUser,
+			micronPass: user.micronPass,
+			userType: user.userType,
+			tenantEntityId: user.tenantId,
+			userId: user.userId ? user.userId : ""
+		};
+	}
+
+	if (user.userType === 1) {
+		return {
+			username: user.username,
+			password: user.password,
+			micronId: user.micronId,
+			micronUser: user.micronUser,
+			micronPass: user.micronPass,
+			userType: user.userType,
+			clubEntityId: user.clubId,
+			userId: user.userId ? user.userId : ""
+		};
+	}
 }
