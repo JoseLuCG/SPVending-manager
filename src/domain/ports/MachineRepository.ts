fcb4ade29f3fbca4cd131437b5b12b5d8d11bc22
JoseLuCG <joseluisconde@protonmail.com>
2025-06-10 09:32:15 +0200

    Feat(domain): add the `page` parameter to `getAll()` methods

--- src/domain/ports/MachineRepository.ts
+++ src/domain/ports/MachineRepository.ts
@@ -2,7 +2,7 @@ import { MachineApiResponse } from "../entities/api-models/apiResponse";
 import { Machine, MachineApi} from "../entities/models/machine";
 
 export interface MachineRepository {
-    getAllMachines() : Promise<MachineApiResponse>;
+    getAllMachines(page:number) : Promise<MachineApiResponse>;
     findMachineByUuid( machineUuid : string ) : Promise<MachineApi | null >;
     addMachine( machine : Omit<Machine, "machineId" | "state"> ) : Promise<void>;
     modifyMachine( machine : Omit<Machine, "state">) : Promise<void>;
--- src/domain/ports/UserRepository.ts
+++ src/domain/ports/UserRepository.ts
@@ -2,7 +2,7 @@ import { UserApiResponse } from "../entities/api-models/apiResponse";
 import { User, UserApi } from "./../entities/models/user"
 
 export interface UserRepository {
-    getAllUsers() : Promise<UserApiResponse>;
+    getAllUsers(page:number) : Promise<UserApiResponse>;
     findUserByUuid( userUuid : string ) : Promise< UserApi | null >;
     addUser( user : Omit<User, "userId" | "clubName"> ) : Promise<void>;
     modifyUser( user :User) : Promise<void>;
