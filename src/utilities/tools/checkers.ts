16efe24e9dc87527517b82355222bde6b532eff3
JoseLuCG <joseluisconde@protonmail.com>
2025-06-11 13:16:59 +0200

    Feat(tools): create a email validator

--- src/utilities/tools/checkers.ts
+++ src/utilities/tools/checkers.ts
@@ -6,3 +6,9 @@ export function getEntityId(rowData:DataTableValue) {
     if ("userManagerId" in rowData) return rowData.userManagerId;
     if ("machineId" in rowData) return rowData.machineId;
 }
+
+//  Validations:
+export function isValidEmail(email:string):boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]{2,63}\.[a-zA-Z]{2,10}$/;
+    return emailRegex.test(email);
+}
