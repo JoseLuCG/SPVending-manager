09dfca0bad5f8fbd0f27e73bdda1a50e51987119
JoseLuCG <joseluisconde@protonmail.com>
2025-06-12 08:35:54 +0200

    Fix(components): avoid the infinite fetch in  selectors

--- src/presentation/components/SelectClubOptions/SelectClubOptions.tsx
+++ src/presentation/components/SelectClubOptions/SelectClubOptions.tsx
@@ -30,7 +30,7 @@ function SelectClubOptions({onSelectClub, clubName, clubUuid}: SelectClubOptionP
     useEffect(
         ()=>{
             fetchClubs();
-        }
+        },[]
     );
 
     return(
--- src/presentation/components/SelectTenantOptions/SelectTenantOptions.tsx
+++ src/presentation/components/SelectTenantOptions/SelectTenantOptions.tsx
@@ -29,7 +29,7 @@ function SelectTenantOptions({onSelectTenant, tenantName, tenantUuid}: SelectTen
     useEffect(
         ()=>{
             fetchTenants();
-        }
+        },[]
     );
 
     return(
